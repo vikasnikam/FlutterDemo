e099dc079fec0bcd802e8ad69a1633a8856c0c27
vikasnikam <vikasnikam05@gmail.com>
2018-12-30 20:55:51 +0530

    Intial Commit

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+     child: Text
+        ("Welcome to flutter by vikas",
+          textDirection: TextDirection.ltr
+      )
+    )
+  );
+}
