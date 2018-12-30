a301d14e91a1f3582cc34a6a95767b79b14a0bfa
vikasnikam <vikasnikam05@gmail.com>
2018-12-30 22:45:55 +0530

    Third commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
+void main() =>runApp(new MyFlutterApp());
 
-    new MaterialApp(
+
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "My Material App",
-     home: Scaffold(
-       appBar: AppBar(title: Text("My Dashboard"),),
-       body: new Material(
-         color: Colors.amber,
-         child: Center(
-             child: Text
-               ("Welcome to flutter by vikas",
-               textDirection: TextDirection.ltr,
-               style: TextStyle(color: Colors.white,fontSize: 40.0),
-             )
-         ),
-       )
-     ),
+      home: Scaffold(
+          appBar: AppBar(title: Text("My Dashboard"),),
+          body: new Material(
+            color: Colors.amber,
+            child: Center(
+                child: Text
+                  ("Welcome to flutter by vikas",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(color: Colors.white,fontSize: 40.0),
+                )
+            ),
+          )
+      ),
 
-     )
+    );
+  }
 
-
-
-
-  );
 }
