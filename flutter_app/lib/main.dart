9925e92948bd83ef40fe9dd6ab6f11a67bf86f64
vikasnikam <vikasnikam05@gmail.com>
2018-12-30 22:37:09 +0530

    second commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-     child: Text
-        ("Welcome to flutter by vikas",
-          textDirection: TextDirection.ltr
-      )
-    )
+
+    new MaterialApp(
+      title: "My Material App",
+     home: Scaffold(
+       appBar: AppBar(title: Text("My Dashboard"),),
+       body: new Material(
+         color: Colors.amber,
+         child: Center(
+             child: Text
+               ("Welcome to flutter by vikas",
+               textDirection: TextDirection.ltr,
+               style: TextStyle(color: Colors.white,fontSize: 40.0),
+             )
+         ),
+       )
+     ),
+
+     )
+
+
+
+
   );
 }
